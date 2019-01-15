87f08c417b8fd3d9e7c01291a25745b32cb36f7f
Tom Holmes <tholmes59@gmail.com>
2019-01-15 18:01:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if phone.match(/\d+/)
+ if phone.match(/\w\d+/)
    return true
  else 
    return false
