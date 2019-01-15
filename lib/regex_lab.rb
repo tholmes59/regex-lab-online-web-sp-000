1af4751e654b123bd9a99d37378f3ebc85efc7c0
Tom Holmes <tholmes59@gmail.com>
2019-01-15 17:34:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\b[aeiouAEIOU]\w*/) 
+    return true 
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/)
+    true
+  else
+    false 
+  end
 end
 
 def valid_phone_number?(phone)
-
+ if phone.scan(/\d+/)
+   return true
+ else 
+   return false
+ end
 end
